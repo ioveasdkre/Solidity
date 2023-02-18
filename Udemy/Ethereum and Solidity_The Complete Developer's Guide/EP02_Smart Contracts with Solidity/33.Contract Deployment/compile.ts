93fbc0eb883a8100560459ba71535744507c0a73
ERI\ioveasdkre <a96080103A>
2023-02-18 14:14:28 +0800

    Udemy Stephen Grider 48~53

--- Udemy/Ethereum and Solidity_The Complete Developer's Guide/EP02_Smart Contracts with Solidity/33.Contract Deployment/compile.ts
+++ Udemy/Ethereum and Solidity_The Complete Developer's Guide/EP02_Smart Contracts with Solidity/33.Contract Deployment/compile.ts
@@ -7,7 +7,7 @@ const solc = require("solc");
 
 // 建立常數 inboxPath，用來儲存 Inbox.sol 的路徑
 // __dirname 是 Node.js 中的全域變數，代表當前執行檔案所在的目錄路徑
-const inboxPath = path.resolve(__dirname, "Contracts", "Inbox.sol"); // 使用此套件確保各種環境都可執行，例如 Windows 或 Linux
+const inboxPath = path.resolve(__dirname, "contracts", "Inbox.sol"); // 使用此套件確保各種環境都可執行，例如 Windows 或 Linux
 
 // 讀取 Inbox.sol 檔案
 const source = fs.readFileSync(inboxPath, "utf8");
@@ -29,11 +29,14 @@ const input = {
   },
 };
 
-// console.log(
-//   JSON.parse(solc.compile(JSON.stringify(input))).contracts["Inbox.sol"].Inbox
-// );
+const output = JSON.parse(solc.compile(JSON.stringify(input)));
+const contract = output.contracts["Inbox.sol"].Inbox;
+const contractABI = contract.abi;
+const contractBytecode = contract.evm.bytecode.object;
+
+// console.log(contract);
+// console.log(contractABI);
+// console.log(contractBytecode);
 
 // 編譯並將 Inbox.sol 的 ABI 輸出
-module.exports = JSON.parse(solc.compile(JSON.stringify(input))).contracts[
-  "Inbox.sol"
-].Inbox;
+export { contractABI, contractBytecode };
--- Udemy/Ethereum and Solidity_The Complete Developer's Guide/EP02_Smart Contracts with Solidity/33.Contract Deployment/test/Indox.test.ts
+++ Udemy/Ethereum and Solidity_The Complete Developer's Guide/EP02_Smart Contracts with Solidity/33.Contract Deployment/test/Indox.test.ts
@@ -1,16 +1,49 @@
-// import assert from "assert";
+import assert from "assert";
+import { contractABI, contractBytecode } from "../compile";
 const ganache = require("ganache");
 const Web3 = require("web3");
-const web3 = new Web3(ganache.provider());
 
+let from: string;
+let inbox: any;
+const INITIAL_STRING: string = "Hi there!";
+
+// 在每個測試前初始化
 beforeEach(async () => {
-  // Get a list of all accounts
-  const fetchedAccounts = await web3.eth.getAccounts();
-  console.log(fetchedAccounts);
+  // 使用 Ganache provider 建立 web3
+  const web3 = new Web3(ganache.provider());
 
-  // Use one of those accounts to deploy the contract
+  // 取得所有帳戶
+  const fetchedAccounts = (await web3.eth.getAccounts()) as string[];
+
+  // 從帳戶列表中選擇第一個帳戶作為合約 deploy 的帳戶
+  [from] = fetchedAccounts;
+
+  // 部署合約並將其存儲在變量中
+  inbox = await new web3.eth.Contract(contractABI)
+    .deploy({
+      data: contractBytecode, // 合約代碼
+      arguments: [INITIAL_STRING], // 參數對應 合約的建構子
+    })
+    .send({ from, gas: "1000000" }); // 人員帳號, 氣體限制
 });
 
+// 測試合約部署
 describe("Index", () => {
-  it("deploys a contract", () => {});
+  it("deploys a contract", () => {
+    // console.log(fetchedAccounts);
+    // console.warn(inbox);
+    assert.ok(inbox.options.address); // 確認地址是否存在
+  });
+
+  it("has a default message", async () => {
+    const message = await inbox.methods.message().call(); // 使用合約的 message() 方法取得目前訊息
+    assert.equal(message, INITIAL_STRING); // 驗證目前訊息是否等於預期的 INITIAL_STRING 值 "Hi there!"
+  });
+
+  it("can change the message", async () => {
+    await inbox.methods.setMessage("goodbye").send({ from });
+
+    const message = await inbox.methods.message().call();
+    assert.equal(message, "goodbye");
+  });
 });
